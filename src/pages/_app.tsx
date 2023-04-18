b03376786551de4f467a21f22b9213a403c31bf2
jakubfiglak <jakub.figlak@netguru.pl>
2023-04-18 21:13:42 +0200

    feat: :sparkles:
    protected pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import "~/styles/globals.css";
 
-import { ClerkProvider } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
 import { type AppType } from "next/app";
 import { Josefin_Sans } from "next/font/google";
+import { useRouter } from "next/router";
 
 import { api } from "~/utils/api";
 
@@ -11,11 +17,38 @@ const josefin = Josefin_Sans({
   variable: "--font-josefin",
 });
 
+//  List pages you want to be publicly accessible, or leave empty if
+//  every page requires authentication. Use this naming strategy:
+//   "/"              for pages/index.js
+//   "/foo"           for pages/foo/index.js
+//   "/foo/bar"       for pages/foo/bar.js
+//   "/foo/[...bar]"  for pages/foo/[...bar].js
+const publicPages = ["/"];
+
 const MyApp: AppType = ({ Component, pageProps }) => {
+  // Get the pathname
+  const { pathname } = useRouter();
+
+  // Check if the current route matches a public page
+  const isPublicPage = publicPages.includes(pathname);
+
+  // If the current route is listed as public, render it directly
+  // Otherwise, use Clerk to require authentication
   return (
     <ClerkProvider {...pageProps}>
       <main className={`${josefin.variable} font-sans`}>
-        <Component {...pageProps} />
+        {isPublicPage ? (
+          <Component {...pageProps} />
+        ) : (
+          <>
+            <SignedIn>
+              <Component {...pageProps} />
+            </SignedIn>
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
+          </>
+        )}
       </main>
     </ClerkProvider>
   );
