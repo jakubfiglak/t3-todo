1442837fec88c08ba4821ac3eb6db79a5f5ff19b
jakubfiglak <jakub.figlak@netguru.pl>
2023-04-18 20:54:33 +0200

    chore: :heavy_plus_sign:
    add import sort eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,7 +18,7 @@ const config = {
   parserOptions: {
     project: path.join(__dirname, "tsconfig.json"),
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "simple-import-sort"],
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
     "@typescript-eslint/consistent-type-imports": [
@@ -29,6 +29,8 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
   },
 };
 
