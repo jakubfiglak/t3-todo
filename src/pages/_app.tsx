364b7303d002402840fbe48c9ee674ea2e2132ea
jakubfiglak <jakub.figlak@netguru.pl>
2023-04-18 20:53:44 +0200

    feat: :sparkles: set up josefin google font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,22 @@
+import "~/styles/globals.css";
+
+import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
+import { Josefin_Sans } from "next/font/google";
 
 import { api } from "~/utils/api";
 
-import "~/styles/globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  variable: "--font-josefin",
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
+      <main className={`${josefin.variable} font-sans`}>
+        <Component {...pageProps} />
+      </main>
     </ClerkProvider>
   );
 };
